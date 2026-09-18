e960c5aef6d860b73290010171a733c360680a1a
karrapriyanka016 <karra.25bai11430@vitbhopal.ac.in>
2026-09-18 21:33:58 +0530

    Delete MaintenanceRecord.java

--- MaintenanceRecord.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.time.LocalDate;
-
-public record MaintenanceRecord(
-        String machineId,
-        LocalDate date,
-        String technician,
-        String workPerformed) {}
