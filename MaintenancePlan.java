1cf96e97a978a84d55da8874b7d5a46381991d14
karrapriyanka016 <karra.25bai11430@vitbhopal.ac.in>
2026-09-18 21:33:24 +0530

    Delete MaintenancePlan.java

--- MaintenancePlan.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public record MaintenancePlan(String machineId, int intervalHours, int warningHours) {
-    public MaintenancePlan {
-        if (intervalHours <= 0) throw new IllegalArgumentException("Interval must be positive.");
-        if (warningHours < 0 || warningHours >= intervalHours)
-            throw new IllegalArgumentException("Warning hours must be smaller than interval.");
-    }
-}
